72b21f20ccebe0c5e41eb8d2ae150179a87df4e9
Robbie Ferguson <contact@category5.tv>
2018-02-05 11:46:50 -0500

    Make core count dynamic

--- zram.sh
+++ zram.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-modprobe zram num_devices=4
+cores=$(nproc --all)
+modprobe zram num_devices=$cores
+
+swapoff -a
 
 totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
-mem=$(( ($totalmem / 4)* 1024))
+mem=$(( ($totalmem / $cores)* 1024 ))
 
-echo $mem > /sys/block/zram0/disksize
-echo $mem > /sys/block/zram1/disksize
-echo $mem > /sys/block/zram2/disksize
-echo $mem > /sys/block/zram3/disksize
-
-mkswap /dev/zram0
-mkswap /dev/zram1
-mkswap /dev/zram2
-mkswap /dev/zram3
-
-swapon -p 5 /dev/zram0
-swapon -p 5 /dev/zram1
-swapon -p 5 /dev/zram2
-swapon -p 5 /dev/zram3
+core=0
+while [ $core -lt $cores ]; do
+  echo $mem > /sys/block/zram$core/disksize
+  mkswap /dev/zram$core
+  swapon -p 5 /dev/zram$core
+  let core=core+1
+done
