9575d7ce470f4514eaee8b6a459210f730e34fbe
Dictator Mei <dctxmei@gmail.com>
2018-10-05 13:34:20 +0900

    Site updated: 2018-10-05 13:34:20

--- zram.sh
+++ zram.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
-cores=$(nproc --all)
-modprobe zram num_devices=$cores
 
+export LANG=C
+
+cores=$(nproc --all)
+
+# disable zram
+core=0
+while [ $core -lt $cores ]; do
+    if [[ -b /dev/zram$core ]]; then
+        swapoff /dev/zram$core
+    fi
+    let core=core+1
+done
+if [[ -n $(lsmod | grep zram) ]]; then
+    rmmod zram
+fi
+
+# disable all
 swapoff -a
 
-totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
-mem=$(( ($totalmem / $cores)* 1024 ))
+# enable zram
+modprobe zram num_devices=$cores
+
+echo lz4 > /sys/block/zram0/comp_algorithm
+
+totalmem=$(free | grep -e "^Mem:" | awk '{print $2}')
+#mem=$(( ($totalmem / $cores)* 1024 ))
+mem=$(( $totalmem * 1024 ))
 
 core=0
 while [ $core -lt $cores ]; do
-  echo $mem > /sys/block/zram$core/disksize
-  mkswap /dev/zram$core
-  swapon -p 5 /dev/zram$core
-  let core=core+1
+    echo $mem > /sys/block/zram$core/disksize
+    mkswap /dev/zram$core
+    swapon -p 5 /dev/zram$core
+    let core=core+1
 done
