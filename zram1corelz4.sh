a6676ab00e8b4031f2aa4f9fc2a369c516f5dac4
yutayu <44407536+yutayu@users.noreply.github.com>
2019-02-23 19:50:56 +0900

    Update zram1corelz4.sh

--- zram1corelz4.sh
+++ zram1corelz4.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-cores=1
 rmmod zram
-modprobe zram num_devices=$cores
+modprobe zram num_devices=1
 
 swapoff -a
 
 echo 'lz4' > /sys/block/zram0/comp_algorithm
 totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
-mem=$(( ($totalmem / $cores)* 1024 ))
+mem=$(( ($totalmem / 1)* 1024 ))
 
-core=0
-while [ $core -lt $cores ]; do
-  echo $mem > /sys/block/zram$core/disksize
-  mkswap /dev/zram$core
-  swapon -p 5 /dev/zram$core
-  let core=core+1
-done
+echo $mem > /sys/block/zram0/disksize
+mkswap /dev/zram$core
+swapon -p 5 /dev/zram0
+
